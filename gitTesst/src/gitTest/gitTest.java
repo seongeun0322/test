58de8131b757848f9b79d47c223d11b7834d63f1
tj-bu <tj-bu@DESKTOP-1DNJJMM>
2022-07-28 16:43:46 +0900

    첫번째 git 실습

---
+++ gitTesst/src/gitTest/gitTest.java
@@ -0,0 +1,20 @@
+package gitTest;
+
+public class gitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int i;
+		int k;
+		
+		for(i=2; i<=9; i++) {
+			System.out.println(i+"´Ü");
+			for(k=1; k<=9; k++) { 
+				System.out.printf("%d X %d = %d \n", i,k,i*k);
+			}
+			
+		}
+	}
+
+}
